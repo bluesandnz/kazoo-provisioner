7a51f8d38ed1496d6769a9df8e659471b4ab4a8b
John Duffield <github@brigid.co.uk>
2014-10-01 09:03:58 +1300

    Create phone.php

---
+++ yealink/txxp72/phone.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * Yealink txxp72 rules File
+ *
+ * @author Andrew Nagy
+ * @author Francis Genet
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_yealink_txxp72_phone extends endpoint_yealink_base {
+    public function __construct(&$config_manager) {
+        parent::__construct($config_manager);
+    }
+
+    function prepareConfig() {
+        parent::prepareConfig();
+    }
+}
+
+?>
